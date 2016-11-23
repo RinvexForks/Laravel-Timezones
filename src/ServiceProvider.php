50472a25173315547c65f26473463b22b30a5d2e
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 14:20:52 +0200

    Applied fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Timezones.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Timezones;
 
 use BrianFaust\ServiceProvider\ServiceProvider as BaseProvider;
